b4f93ebdb3bdeaf7e4d8db6224ac236880c09d60
stelianchioveanu <stelian_chioveanu@yahoo.com>
2024-04-17 17:24:09 +0300

    added role extraction from jwt

--- MobyLabWebProgramming.Infrastructure/Authorization/AuthorizedController.cs
+++ MobyLabWebProgramming.Infrastructure/Authorization/AuthorizedController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using MobyLabWebProgramming.Core.DataTransferObjects;
+using MobyLabWebProgramming.Core.Enums;
 using MobyLabWebProgramming.Core.Responses;
 using MobyLabWebProgramming.Infrastructure.Services.Interfaces;
 
@@ -31,8 +32,9 @@ public abstract class AuthorizedController : ControllerBase
         var userId = enumerable.Where(x => x.Type == ClaimTypes.NameIdentifier).Select(x => Guid.Parse(x.Value)).FirstOrDefault();
         var email = enumerable.Where(x => x.Type == ClaimTypes.Email).Select(x => x.Value).FirstOrDefault();
         var name = enumerable.Where(x => x.Type == ClaimTypes.Name).Select(x => x.Value).FirstOrDefault();
+        var role = enumerable.Where(x => x.Type == "Role").Select(x => UserRoleEnum.FromValue(x.Value)).FirstOrDefault();
 
-        _userClaims = new(userId, name, email);
+        _userClaims = new(userId, name, email, role);
 
         return _userClaims;
     }
